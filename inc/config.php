686a3e239484f28d41f62f142ae6d2d78cf9dcd5
excitedcrayon <bonganimkonto@gmail.com>
2018-08-05 15:00:37 +1000

    Changes to inc/config.php file

--- inc/config.php
+++ inc/config.php
@@ -4,7 +4,7 @@
 // enters uppercase or lowercase variable from definition
 // e.g db_host or DB_HOST
 define('db_host','localhost', true);
-define('db_user','file');
-define('db_password', 'upload');
+define('db_user',''); // change this to your own database username
+define('db_password', ''); // change this to your own database password
 define('db_name', 'file_upload')
 ?>
